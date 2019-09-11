238a5f64571995cd66499dd987344af46c82fe22
carlo11able <andreabaroni1909@gmail.com>
2019-09-11 23:56:28 +0200

    Add authentication with devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+    #protect_from_forgery with: :null_session        #annulla CSRF token authentication
+    before_action :authenticate_user!
+
+    #skip_before_action :verify_authenticity_token      #another way to deactivate CSRF token authentication
+
+    def index
+        @users=User.all()
+        #render :html => "prova"
+    end
+    
+
+    def show
+    end
+
+    def new
+    end
+
+    def create 
+    end
+
+    def edit     
+    end
+
+    def update
+    end
+    
+
+    
+
+    def destroy
+    end
+end
